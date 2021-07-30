13abcbffb01b730012f9724e55fb675f962ae284
Frederic Rosselet <fred.rosselet@gmail.com>
2021-07-30 15:23:45 -0700

    finished load testing (GET)

--- k6/script.js
+++ k6/script.js
@@ -1,21 +1,42 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-const BASE_URL = 'http://localhost:3001';
+// const sampleDataForOneProduct = {
+//   cast: [
+//     'Jordan Acevedo',
+//     'Aschale Siyoum',
+//     'Christopher Raffaele',
+//     'Fred Rosselet'
+//   ],
+//   aspectRatio: '2:1',
+//   rating: 'HR',
+//   dimensions: '4 x 0 x 2 Inches',
+//   format: 'Zoom Screen',
+//   runTime: '8 weeks',
+//   studio: 'RPT Studios',
+//   numberOfDisks: 88
+// };
+
+const BASE_URL = 'http://localhost:3000';
 
 export let options = {
   scenarios: {
     constant_request_rate: {
       executor: 'constant-arrival-rate',
-      rate: 1,
+      rate: 1000,
       timeUnit: '1s',
-      duration: '30s',
+      duration: '2m',
       preAllocatedVUs: 100,
-      maxVUs: 200
-    },
-  },
+      maxVUs: 500
+    }
+  }
 };
 
-export default function () {
-  http.get(`${BASE_URL}/dp/9999999`);
+export default () => {
+  const min = 9000000;
+  const max = 10000000;
+  let id = Math.floor(Math.random() * (max - min + 1) + min);
+  http.get(`${BASE_URL}/${id}`);
   sleep(1);
+  // http.post(BASE_URL, JSON.stringify(sampleDataForOneProduct), { headers: {'Content-Type': 'application/json' } });
+  // sleep(1);
 };
