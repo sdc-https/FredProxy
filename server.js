b7cc778a5006e41407273942c2a8768662297aeb
Frederic Rosselet <fred.rosselet@gmail.com>
2021-08-03 20:51:38 -0700

    last commit beofe deployment

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const newrelic = require('newrelic');
 const express = require('express');
 const app = express();
 const path = require('path');
-// const bodyParser = require('body-parser');
-// const axios = require('axios');
+const morgan = require('morgan');
 const createProxyMiddleware = require('http-proxy-middleware');
 
 const PORT = 3000;
 const API_PRODUCT_INFORMATION_URL = 'http://localhost:3001';
 
-// app.use(bodyParser.json());
-
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
@@ -32,19 +30,6 @@ app.get('/:productId', (req, res) => {
 app.use('/Information', createProxyMiddleware({
   target: API_PRODUCT_INFORMATION_URL
 }));
-
-// app.get('/Information/:productId', (req, res) => {
-//   axios.get(`${API_PRODUCT_INFORMATION_URL}/${req.params.productId}`)
-//     .then((results) => res.json(results.data))
-//     .catch((err) => console.log('Product Information GET error:', err));
-// });
-
-// app.post('/Information', (req, res) => {
-//   let data = req.body;
-//   axios.post(API_PRODUCT_INFORMATION_URL, data)
-//     .then((result) => res.json(result.data))
-//     .catch((error) => console.log('Product Information POST error:', error));
-// });
 
 
 
